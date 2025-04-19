26510973482049ea761ecdd328ccf1325c3a3d24
abhidatta0 <abhidatta146@gmail.com>
2025-04-20 00:00:00 +0530

    fixed : vite build to use .js also

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,28 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   // ...
-  plugins:[react()],
+  plugins:[
+    {
+      name: 'load+transform-js-files-as-jsx',
+      async transform(code, id) {
+        if (!id.match(/src\/.*\.js$/)) {
+          return null;
+        }
+
+        // Use the exposed transform from vite, instead of directly
+        // transforming with esbuild
+        return transformWithEsbuild(code, id, {
+          loader: 'jsx',
+          jsx: 'automatic',
+        });
+      },
+    },
+    react()],
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+      '.js': 'jsx',
+      },
+    },
+  },
 })
